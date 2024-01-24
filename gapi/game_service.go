7d3f707d5677337c7f814a1b09c6f0389cd98007
Oğuzcan Küçükbayrak <o.kucukbayrak@icloud.com>
2024-01-24 23:02:34 +0300

    cant compile

--- gapi/game_service.go
+++ gapi/game_service.go
@@ -17,19 +17,18 @@ type GameService struct {
 
 func (s *GameService) CreateGame(ctx context.Context, in *gamepb.CreateGameRequest) (*gamepb.GameIdResponse, error) {
 	userID := getUserIdFromCtx(ctx)
-	query := `INSERT INTO game (
+	query := `INSERT INTO game 
+	(
 		courtID,
 		startedAt,
 		homeScore,
 		awayScore
-	  )
-	VALUES (
+	) VALUES (
 		?,
 		NOW(),
 		0,
 		0
-	  );
-	  `
+	);`
 
 	res, err := s.db.Exec(query, in.GetCourtId())
 	if err != nil {
@@ -95,5 +94,47 @@ func (s *GameService) JoinGame(ctx context.Context, in *gamepb.JoinGameRequest) 
 
 	return &gamepb.GameIdResponse{
 		GameId: in.GetGameId(),
+	}, nil
+}
+
+func (s *GameService) StartGame(ctx context.Context, in *gamepb.StartGameRequest) (*gamepb.GameResponse, error) {
+	query := `UPDATE game SET startedAt = NOW() WHERE id = ?;`
+
+	_, err := s.db.Exec(query, in.GetGameId())
+	if err != nil {
+		fmt.Println(err)
+		return nil, status.Errorf(codes.Internal, "Internal error")
+	}
+
+	query = `SELECT id, homeScore, awayScore, startedAt, endedAt FROM game WHERE id = ?;`
+	row := s.db.QueryRow(query, in.GetGameId())
+
+	var game gamepb.Game{}
+	err = row.Scan(&game.Id, &game.HomeScore, &game.AwayScore, &game.StartedAt, &game.EndedAt)
+
+	if err != nil {
+		return nil, gerr(codes.Internal, err)
+	}
+
+	return nil, status.Errorf(codes.Unimplemented, "method StartGame not implemented")
+}
+
+func (s *GameService) EndGame(ctx context.Context, in *gamepb.EndGameRequest) (*gamepb.GameResponse, error) {
+
+	query := `UPDATE game SET endedAt = NOW(), homeScore = ?, awayScore = ? WHERE id = ?;`
+
+	_, err := s.db.Exec(query, in.GetHomeScore(), in.GetAwayScore(), in.GetGameId())
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, status.Errorf(codes.Internal, "Internal error")
+	}
+
+	var game gamepb.Game
+	query = `SELECT id, homeScore, awayScore, startedAt, endedAt FROM game WHERE id = ?;`
+	s.db.QueryRow(query, in.GetGameId()).Scan(&game.Id, &game.HomeScore, &game.AwayScore, &game.StartedAt, &game.EndedAt)
+
+	return &gamepb.GameResponse{
+		Game: &game,
 	}, nil
 }
